165b77e7ccc75f9655fc8fac01d5f8217b2d4107
codewhisperer <michael@langmayr.net>
2016-01-08 15:04:13 -0800

    fixed build script

--- build.sh
+++ build.sh
@@ -3,13 +3,15 @@
 # Increase file descriptor limit on OSX.
 ulimit -n 2560
 
-# install dependencies
+# install npm packages
 npm i
-bower install
 
 # set path for binaries of node modules
 PATH=node_modules/.bin:$PATH
 export PATH
+
+# install node modules
+bower install --config.interactive=false
 
 # compile front end code
 grunt webpack
